80433d2c67d383702cc5484cffab91d4f402e6c2
Joseph <JosephSchander@gmail.com>
2021-09-11 16:59:21 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        DecimalFormat DF = new DecimalFormat("##.##");
+        Scanner uInput = new Scanner(System.in);
+        System.out.println( "How many euros are you exchanging?");
+        int euros = uInput.nextInt();
+        System.out.println(" What is the exchange rate?");
+        double rate = uInput.nextDouble();
+        double total = (euros * rate);
+
+
+        System.out.println(euros +" euros at an exchange rate of " + rate + " is " + DF.format(total) + " USD");
+    }
+}
